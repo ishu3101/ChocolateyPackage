85bcef0b8a911c5daa020e5b56fd80220a060ca2
Ishan <ishu3101@hotmail.com>
2013-12-07 09:15:00 +0000

    Added Package for Listary.

---
+++ Listary/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'Listary'
+$installerType = 'exe'
+$url = 'http://www.listary.com/download/Listary.exe'
+$silentArgs = '/SP /VERYSILENT /NORESTART'
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
