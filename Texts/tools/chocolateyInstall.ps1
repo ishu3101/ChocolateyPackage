fca9d09b08ed63e45ccbb5dd2fb38983eea2d2b7
Ishan <ishu3101@hotmail.com>
2013-12-07 08:15:00 +0000

    Added Package for Texts.io.

---
+++ Texts/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'Texts'
+$installerType = 'msi'
+$url = 'http://www.texts.io/Texts-0.17.1.msi'
+$silentArgs = '/quiet'
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
