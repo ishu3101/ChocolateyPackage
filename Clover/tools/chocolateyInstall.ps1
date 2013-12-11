2a271ddd84abe3da427917dfbc961c54798c1ed1
Ishan <ishu3101@hotmail.com>
2013-12-11 10:17:00 +0000

    Added Package for Clover.

---
+++ Clover/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'Clover'
+$installerType = 'exe'
+$url = 'http://ejie.me/uploads/Clover_Setup_3.0.386.zip'
+$silentArgs = '/S'
+# $file = ''
+
+# download and unpack a zip file containing the setup
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
+
+# install the application using the extracted setup file 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\clover_setup.exe"
